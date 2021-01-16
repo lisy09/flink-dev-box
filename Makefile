f65cda4a4b8f843a507bb54e21bc5115560bd7c5
lisy09 <lisy09.thu@gmail.com>
2021-01-16 23:33:44 +0900

    github pages

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ endif
 
 BUILD_SCRIPTS_PATH = ./build_scripts
 RUN_SCRIPTS_PATH = ./run_scripts
+
+.PHONY: github-pages
+github-pages:
+	cd dev-docs && make github-pages
--- dev-docs/run_scripts/build_github_pages.sh
+++ dev-docs/run_scripts/build_github_pages.sh
@@ -2,6 +2,7 @@
 
 PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR="$( cd $PARENT_DIR/.. >/dev/null 2>&1 && pwd )"
+TARGET_DOCS_DIR=${ROOT_DIR}/../docs
 
 set -x
 
@@ -14,7 +15,7 @@ docker cp ${ROOT_DIR}/${DOCS_RELATIVE_PATH} $instance:${WOKRKING_PACKAGE_PATH}/$
 docker exec $instance bash -c "cd ${WOKRKING_PACKAGE_PATH} \
     && DOCS_BASEURL=/dev-docs-template/ npx vuepress build ${DOCS_RELATIVE_PATH}"
 
-rm -rf ${ROOT_DIR}/docs
-docker cp $instance:${WOKRKING_PACKAGE_PATH}/${DOCS_RELATIVE_PATH}/.vuepress/dist ${ROOT_DIR}/docs
+rm -rf ${TARGET_DOCS_DIR}
+docker cp $instance:${WOKRKING_PACKAGE_PATH}/${DOCS_RELATIVE_PATH}/.vuepress/dist ${TARGET_DOCS_DIR}
 
 docker stop $instance
